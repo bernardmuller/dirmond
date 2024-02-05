198a2339cf84d861ff63dcc7096dd6b817fa908a
Bernard Muller <b.mullerjnr@gmail.com>
2024-02-05 22:18:00 +0200

    feat: monitor directory, watch for file creation events

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,90 @@
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
 use std::ffi::OsStr;
+use std::fs;
 use std::path::Path;
 use std::time::SystemTime;
-use std::{env, fs};
-
-fn get_dir_from_env(key: &str) -> String {
-    match env::var(key) {
-        Ok(val) => val,
-        Err(err) => panic!("{}", err),
-    }
-}
-
-fn get_extension_from_filename(filename: &str) -> String {
-    match Path::new(filename).extension().and_then(OsStr::to_str) {
-        Some(extension) => format!(".{}", extension),
-        _ => String::from("no extension"),
-    }
-}
 
 fn main() {
-    let directory: &str = &get_dir_from_env("DIR");
-    const IGNORE_FILES: [&str; 2] = [".DS_Store", ".localized"];
+    let path = std::env::args()
+        .nth(1)
+        .expect("Argument 1 needs to be a path");
+    println!("watching {}", path);
+    if let Err(e) = watch(path) {
+        println!("error: {:?}", e)
+    }
+}
 
-    let paths = fs::read_dir(directory);
-    match paths {
-        Ok(paths) => {
-            paths.for_each(|path| {
-                let path = path.unwrap().path();
-                let file_name = path.file_name().unwrap().to_str().unwrap();
-                let meta_data = fs::metadata(&path).unwrap();
-                let file_type = get_extension_from_filename(&file_name);
-                let current_time = SystemTime::now();
-                let file_time_created = meta_data.created().unwrap();
-                let time_diff = current_time
-                    .duration_since(file_time_created)
-                    .unwrap()
-                    .as_secs()
-                    / 60
-                    / 60
-                    / 24;
-                if !IGNORE_FILES.contains(&file_name) {
-                    println!("Filename: {}", file_name);
-                    println!("File type: {:#?}", file_type);
-                    println!("Created {:#?} days ago", time_diff);
-                }
-            });
-        }
-        Err(e) => {
-            println!("Error: {}", e);
+fn watch(path: String) -> notify::Result<()> {
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+
+    for res in rx {
+        match res {
+            Ok(event) => {
+                match event.kind {
+                    notify::EventKind::Access(_) => {}
+                    notify::EventKind::Create(_) => {
+                        println!("File Created");
+                        println!("===============================");
+
+                        let dir_files = fs::read_dir(&path);
+                        match dir_files {
+                            Ok(files) => {
+                                files.for_each(|file| match file {
+                                    Ok(file) => {
+                                        let event_path_os_file_name: &OsStr =
+                                            &event.paths[0].file_name().unwrap();
+                                        if let Some(event_path) = event_path_os_file_name.to_str() {
+                                            let pth = file.path();
+                                            let file_path = Path::new(pth.to_str().unwrap())
+                                                .file_name()
+                                                .unwrap()
+                                                .to_str()
+                                                .unwrap();
+
+                                            if &event_path.get(1..).unwrap() == &file_path {
+                                                let meta_data = fs::metadata(&file.path()).unwrap();
+                                                let current_time = SystemTime::now();
+                                                let file_time_created =
+                                                    meta_data.created().unwrap();
+                                                let time_diff = current_time
+                                                    .duration_since(file_time_created)
+                                                    .unwrap()
+                                                    .as_secs()
+                                                    / 60
+                                                    / 60
+                                                    / 24;
+                                                println!("File: {:?}", file.file_name());
+                                                println!("Path: {:?}", file.path());
+                                                println!("Created {:#?} days ago", time_diff);
+                                                println!(
+                                                    "Metadata: {:?}",
+                                                    fs::metadata(&file.path()).unwrap()
+                                                );
+                                            };
+                                        }
+                                    }
+                                    Err(e) => {
+                                        println!("Error: {}", e);
+                                    }
+                                });
+                            }
+                            Err(e) => {
+                                println!("Error: {}", e);
+                            }
+                        }
+                    }
+                    notify::EventKind::Modify(_) => {}
+                    notify::EventKind::Remove(_) => {}
+                    notify::EventKind::Other => {}
+                    notify::EventKind::Any => {}
+                };
+                print!("")
+            }
+            Err(e) => println!("watch error: {:?}", e),
         }
     }
+
+    Ok(())
 }
