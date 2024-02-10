55ac8adf9f4b9a9726298433ae54db1781d118c4
Bernard Muller <b.mullerjnr@gmail.com>
2024-02-10 12:57:38 +0200

    feat: cleaner, watcher threads with event handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
 use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use notify_rust::Notification;
 use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
+use std::thread;
+use std::time::Duration;
 use std::time::SystemTime;
 
+const IGNORED_FILES: [&str; 2] = [".DS_Store", ".gitignore"];
+const WHITELISTED_FILES: [&str; 1] = ["png"];
+
+fn get_args() -> Vec<String> {
+    std::env::args().collect()
+}
+
 fn main() {
-    let path = std::env::args()
-        .nth(1)
-        .expect("Argument 1 needs to be a path");
-    println!("watching {}", path);
-    if let Err(e) = watch(path) {
-        println!("error: {:?}", e)
-    }
+    print!("{}[2J", 27 as char);
+    let cleaner_thread = thread::spawn(|| {
+        let args = get_args();
+        let path = args[1].clone();
+        cleaner(&path);
+    });
+
+    let watcher_thread = thread::spawn(|| {
+        let args = get_args();
+        let path = args[1].clone();
+        if let Err(e) = watch(&path) {
+            println!("error: {:?}", e)
+        }
+    });
+
+    cleaner_thread.join().unwrap();
+    watcher_thread.join().unwrap();
 }
 
 #[derive(Debug)]
@@ -21,6 +41,91 @@ struct File {
     created: SystemTime,
     modified: SystemTime,
     size: u64,
+    file_type: String,
+}
+
+fn file_type_is_whitelisted(file: &File) -> bool {
+    if WHITELISTED_FILES.contains(&file.file_type.as_str()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+fn file_is_whitelisted(file: &File) -> bool {
+    if WHITELISTED_FILES.contains(&file.name.as_str()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+fn move_file_to_bin(file: &File) {
+    println!("Moving File {} to bin", file.name);
+    trash::delete(&file.path).unwrap();
+}
+
+fn delete_file(file: &File) {
+    fs::remove_file(&file.path).unwrap()
+}
+
+fn cleaner(path: &str) {
+    println!("Cleaning: {}", &path);
+    loop {
+        let files = get_files_in_dir(&path);
+        match files {
+            Ok(files) => {
+                files.iter().for_each(|file| {
+                    // println!("File: {:#?}", file);
+
+                    match file.modified.elapsed() {
+                        Ok(elapsed) => {
+                            if elapsed.as_secs() > 10 && file_type_is_whitelisted(&file) {
+                                delete_file(&file);
+                                // move_file_to_bin(&file);
+                            }
+                        }
+                        Err(e) => {
+                            println!("Error: {}", e);
+                        }
+                    }
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+fn get_files_in_dir(path: &str) -> Result<Vec<File>, std::io::Error> {
+    let dir_files = fs::read_dir(&path);
+    match dir_files {
+        Ok(files) => {
+            let mut file_vec: Vec<File> = Vec::new();
+            files.for_each(|file| match file {
+                Ok(file) => {
+                    if file.path().is_dir() {
+                        return;
+                    }
+                    if IGNORED_FILES.contains(&file.path().file_name().unwrap().to_str().unwrap()) {
+                        return;
+                    }
+                    let file = get_file_info(&file.path().to_str().unwrap());
+                    file_vec.push(file);
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
+            });
+            return Ok(file_vec);
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+            return Err(e);
+        }
+    }
 }
 
 fn get_file_info(path: &str) -> File {
@@ -28,17 +133,38 @@ fn get_file_info(path: &str) -> File {
     let file_time_created = meta_data.created().unwrap();
     let file_time_modified = meta_data.modified().unwrap();
     let file_size = meta_data.len();
+    let file_name = Path::new(path).file_name().unwrap().to_str().unwrap();
     File {
-        name: path.to_string(),
+        name: file_name.to_string(),
         path: path.to_string(),
         created: file_time_created,
         modified: file_time_modified,
         size: file_size,
+        file_type: file_name
+            .split('.')
+            .collect::<Vec<&str>>()
+            .pop()
+            .unwrap()
+            .to_string(),
     }
 }
 
 fn handle_create_event(event: &notify::Event, path: &str) {
+    let file = get_mofified_file(&event, &path);
+    let notification_string = format!("{} was created", file.name);
+    println!("{}", notification_string);
+}
+
+fn get_mofified_file(event: &notify::Event, path: &str) -> File {
     let dir_files = fs::read_dir(&path);
+    let mut modified_file: File = File {
+        name: "".to_string(),
+        path: "".to_string(),
+        created: SystemTime::now(),
+        modified: SystemTime::now(),
+        size: 0,
+        file_type: "".to_string(),
+    };
     match dir_files {
         Ok(files) => {
             files.for_each(|file| match file {
@@ -53,8 +179,7 @@ fn handle_create_event(event: &notify::Event, path: &str) {
                             .unwrap();
 
                         if &event_path.get(1..).unwrap() == &file_path {
-                            let file = get_file_info(&file.path().to_str().unwrap());
-                            println!("File: {:#?}", file);
+                            modified_file = get_file_info(&file.path().to_str().unwrap());
                         };
                     }
                 }
@@ -67,9 +192,36 @@ fn handle_create_event(event: &notify::Event, path: &str) {
             println!("Error: {}", e);
         }
     }
+    return modified_file;
 }
 
-fn watch(path: String) -> notify::Result<()> {
+fn handle_modify_event(event: &notify::Event, path: &str) {
+    let modified_file = get_mofified_file(&event, &path);
+    if !file_type_is_whitelisted(&modified_file) {
+        return;
+    }
+    // let event_metadata = event.
+    let notification_string = format!("{} was modified", modified_file.name);
+
+    println!("{}", notification_string);
+    Notification::new()
+        .summary("File Modified")
+        .body(&notification_string)
+        .show()
+        .unwrap();
+}
+
+fn handle_remove_event(path: &str) {
+    let notification_string = format!("A file was removed from {}", &path);
+    Notification::new()
+        .summary("File Removed")
+        .body(&notification_string)
+        .show()
+        .unwrap();
+}
+
+fn watch(path: &str) -> notify::Result<()> {
+    println!("Watching: {}", &path);
     let (tx, rx) = std::sync::mpsc::channel();
     let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
     watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
@@ -82,8 +234,12 @@ fn watch(path: String) -> notify::Result<()> {
                     notify::EventKind::Create(_) => {
                         handle_create_event(&event, &path);
                     }
-                    notify::EventKind::Modify(_) => {}
-                    notify::EventKind::Remove(_) => {}
+                    notify::EventKind::Modify(_) => {
+                        // handle_modify_event(&event, &path);
+                    }
+                    notify::EventKind::Remove(_) => {
+                        handle_remove_event(&path);
+                    }
                     notify::EventKind::Other => {}
                     notify::EventKind::Any => {}
                 };
